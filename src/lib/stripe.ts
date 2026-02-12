8489d2c071fc0db46afd2818bfa81468905b8c3e
Mohammed <you@example.com>
2026-02-12 11:07:42 +0200

    edit stripe.ts file

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,3 +1,10 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('Missing STRIPE_SECRET_KEY environment variable');
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2025-11-17.clover',
+  typescript: true,
+})
